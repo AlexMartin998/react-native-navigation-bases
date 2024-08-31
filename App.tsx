8b625f70e5b3e507f200a1571c289e2faa4d209b
Adrian <alextravc.342@gmail.com>
2024-08-31 11:48:01 -0500

    ADD: react navigator STACK intial config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import './src/config/navigation/gesture-handler.native';
+
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Text} from 'react-native';
+import {StackNavigator} from './src/presentation/routes/StackNavigator';
 
 export const App = () => {
   return (
     // Navigation already has a SafeAreaView
     <NavigationContainer>
-      <Text>App</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
