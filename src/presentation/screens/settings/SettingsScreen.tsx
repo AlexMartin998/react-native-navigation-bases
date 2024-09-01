593af8817aaf6ff79871255065097aaf2fb066f3
Adrian <alextravc.342@gmail.com>
2024-08-31 22:18:35 -0500

    ADD: navigate go back & popToTop (first screen in the stack)

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {StackActions, useNavigation} from '@react-navigation/native';
+
+import {GlobalStyles} from '../../../config/theme/theme';
+import {PrimaryButton} from '../../components/shared';
+import {NavigationRootPropType} from '../../routes';
 
 export const SettingsScreen = () => {
+  const navigator = useNavigation<NavigationRootPropType>();
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={GlobalStyles.container}>
+      <Text style={GlobalStyles.title}>SettingsScreen</Text>
+
+      <View style={styles.btnContainer}>
+        <PrimaryButton
+          label="Go back"
+          onPress={() => {
+            // navigate to the previous screen
+            navigator.goBack();
+          }}
+        />
+
+        <PrimaryButton
+          label="Go to Home"
+          onPress={() => {
+            // navigate to the 1st screen in the stack
+            navigator.dispatch(StackActions.popToTop());
+          }}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    gap: 10,
+  },
+});
